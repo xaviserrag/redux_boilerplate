9ad4836b1e6102a8d6f49c109e2c5fb3b74724f9
Xavi Serra <xaviserrag@gmail.com>
2017-01-24 21:14:16 -0800

    Added node_modules to gitignore, Added linter

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,42 @@
+/* eslint-disable no-console, import/no-extraneous-dependencies */
+
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
+import eslint from 'gulp-eslint';
 import { exec } from 'child_process';
 
 const paths = {
-    allSrcJs: 'src/**/*.js',
-    libDir: 'lib'
+  allSrcJs: 'src/**/*.js',
+  gulpFile: 'gulpfile.babel.js',
+  libDir: 'lib',
 };
 
-gulp.task('clean', () => {
-    return del(paths.libDir);
-});
+gulp.task('clean', () => del(paths.libDir));
 
-gulp.task('build', ['clean'], () => {
-    return gulp.src(paths.allSrcJs)
-        .pipe(babel())
-        .pipe(gulp.dest(paths.libDir));
-});
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir))
+);
 
 gulp.task('main', ['build'], (callback) => {
-    exec(`node ${paths.libDir}`, (error, stdout) => {
-        console.log(stdout);
-        return callback(error);
-    })
+  exec(`node ${paths.libDir}`, (error, stdout) => {
+    console.log(stdout);
+    return callback(error);
+  });
 });
 
-gulp.task('watch', () => {
-    gulp.watch(paths.allSrcJs, ['main']);
-});
+gulp.task('watch', () => gulp.watch(paths.allSrcJs, ['main']));
+
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
 
 gulp.task('default', ['watch', 'main']);
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const str = 'ES6'
-
-console.log(`Hello ${str}`);
